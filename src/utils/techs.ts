07155a1c3d1ea60dfed655f8916443d82a8f70e5
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2023-10-20 12:26:25 -0300

    add redux icon in skills

--- src/utils/techs.ts
+++ src/utils/techs.ts
@@ -1,8 +1,9 @@
 export const techsUrl = {
     Javascript: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
     Typescript: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
-    React: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
     NextJs: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg',
+    React: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    Redux: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
     Node: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
     MongoDB: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
     PostgreSQL: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
