b61f1279e5bb0ebe88c009c57edaa7fd50c0636c
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2024-08-08 17:19:24 -0300

    "Updated next.config.js to use remotePatterns for images, and modified CSS styles in SelectCategorys.module.css and globals.css to adjust select categorys"

--- next.config.js
+++ next.config.js
@@ -4,7 +4,25 @@ const nextConfig = {}
 // next.config.js
 module.exports = {
     images: {
-      domains: ['cdn.jsdelivr.net', 'viz.mediapipe.dev', 'devicon-website.vercel.app', 'img.icons8.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.jsdelivr.net',
+        },
+        {
+          protocol: 'https',
+          hostname: 'viz.mediapipe.dev',
+        },
+        {
+          protocol: 'https',
+          hostname: 'devicon-website.vercel.app',
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.icons8.com',
+        },
+      ],
+      /* domains: ['cdn.jsdelivr.net', 'viz.mediapipe.dev', 'devicon-website.vercel.app', 'img.icons8.com'], */
     }
   };
   
