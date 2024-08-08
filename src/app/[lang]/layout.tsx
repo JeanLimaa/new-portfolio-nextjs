b9fdb474bd9ee55f94b700f717ed5efad1c887b2
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2024-08-07 22:38:25 -0300

    adjust metadata

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -7,8 +7,9 @@ import { Oxanium } from 'next/font/google'
 const oxanium = Oxanium({ subsets: ['latin'], weight: ["300", "600", "400", "500", "600", "700"]})
 
 export const metadata: Metadata = {
-  title: 'Portfolio - Jean',
-  description: 'Portfolio de Jean Lima, desenvolvedor fullstack javascript.',
+  title: 'Portfolio - Jean Lima',
+  description: 'Conheça o portfolio de Jean Lima, um desenvolvedor fullstack especializado em Javascript, com experiência em React, Next.js, Node.js e outras tecnologias modernas.',
+  keywords: 'Jean Lima, desenvolvedor fullstack, Javascript, React, Next.js, Node.js, portfolio, desenvolvedor web, desenvolvimento web, programação',
   icons: '/icon.png'
 }
 
